ada87a9c35e830904e956d6d3258d24bcb18757d
sanchahous <sanchahous@gmail.com>
2017-03-28 10:16:15 +0300

    js fix

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 /**
  * Created by sanchahous on 28.03.2017.
  */
-$('#countdown').countdown('2017/03/28 10:00:00').on('update.countdown', function(event) {
+$('#countdown').countdown('2017/03/28 19:00:00').on('update.countdown', function(event) {
     var $this = $(this).html(event.strftime(''
         + ' <div class="top-line__timer top-line__hour"> <span>%H</span> <span>часов</span> <span class="top-line__timer__points">:</span></div>'
         +  '<div class="top-line__timer top-line__min"> <span>%M</span> <span>минут</span> <span class="top-line__timer__points">:</span> </div>'
